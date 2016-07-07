f97d25aa5c27f453bf6551d9f3c3b245ad61a38e
Chip Boling <chip@bcsw.net>
2016-07-07 09:46:56 -0500

    Initial commit for adtran CORD configuration

---
+++ adtn-cord/os-keys/cboling-devstack-mitaka-admin-openrc.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Below are the Chip's devstack-mitaka @ adtran Credentials, replace with what you really need
+
+export OS_TENANT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=devstack
+export OS_AUTH_URL=http://192.168.56.121:35357/v2.0
+
---
+++ adtn-cord/os-keys/pod5-admin-openrc.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Below are the POD 5 Credentials, replace with what you really need
+
+export OS_TENANT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=stack
+export OS_AUTH_URL=http://pod5-controller:35357/v2.0
+
